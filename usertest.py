f7c7c63f33091ac33e86705686c7febde2b17886
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2018-09-23 01:09:06 +0530

    Encryption Code

--- usertest.py
+++ usertest.py
@@ -1,14 +1,13 @@
-from helpers import PropertyHelper;
-import base64;
-import mysql.connector;
-
-config = PropertyHelper();
-print(base64.b64encode(b'root'));
-
-username = base64.b64decode(config.props['username'])
-password = base64.b64decode(config.props['password']);
-host = config.props['host'];
-port = config.props['port']
-dbname = config.props['dbname']
-conn = mysql.connector.connect(user=username, password=password , host=host, port=port, database=dbname);
-print(conn.is_connected());
+# pip install cryptography
+# this is symmetric encyrption =#= i am encrypting using the samekey
+# i am decrypting also using the same key
+from cryptography.fernet import Fernet;
+key = Fernet.generate_key();
+print(key)
+#keyfile = open("d:/key.txt", 'wt');
+#keyfile.write(key);
+# first read the key
+cipher_suite = Fernet(key);
+# encrypt of decrypt
+encrypted_text = cipher_suite.encrypt(b'this is a secret');
+decrypted_text = cipher_suite.decrypt(encrypted_text);
